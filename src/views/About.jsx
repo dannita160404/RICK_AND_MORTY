630215b254eff6d13b61fe55394f810ecf596239
dannita160404 <sofiadryp@gmail.com>
2023-10-07 00:00:48 -0500

    proyecto-_RICK_AND_MORTY

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -4,6 +4,7 @@ const About = ( ) => {
             <h3>BIENVENIDO A MI APP</h3>
             
             <p>APP RICK AND MORTY</p>
+            <hr/>
             <h2>Bienvenido a mi app de Rick and morty
       
             espero te guste y sea de una experiencia de agrado para ti muchas gracias</h2>
